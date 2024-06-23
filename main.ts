d356f17686524a86603a071979caf29f36b160f5
Elias Jaffe <elijaffe173@gmail.com>
2024-06-23 03:28:43 -0400

    Initial Commit

--- main.ts
+++ main.ts
@@ -1,134 +1,168 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+// https://x.com/LegacyRussell/status/1366584145271488522
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface RemoveNewlineSettings {
+	fixWhitespace: boolean;
+	fixHyphenation: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: RemoveNewlineSettings = {
+	fixWhitespace: true,
+	fixHyphenation: true,
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class RemoveNewline extends Plugin {
+	settings: RemoveNewlineSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
+			id: "remove-newlines-from-selection",
+			name: "Remove newlines from selection",
+			editorCheckCallback(checking, editor, ctx) {
+				if (editor.somethingSelected()) {
 					if (!checking) {
-						new SampleModal(this.app).open();
+						this.removeNewlinesFromSelection(editor);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
-			}
+				return false;
+			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addCommand({
+			id: "paste-without-newlines",
+			name: "Paste without newlines",
+			editorCallback(editor) {
+				this.pasteWithoutNewlines(editor);
+			},
+		});
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				menu.addItem((item) => {
+					item.setTitle("Remove newlines from selection")
+						.setIcon("square-bottom-dashed-scissors")
+						.setDisabled(!editor.somethingSelected())
+						.onClick(() => {
+							this.removeNewlinesFromSelection(editor);
+						});
+				});
+			})
+		);
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				menu.addItem((item) => {
+					item.setTitle("Paste without newlines")
+						.setIcon("clipboard-paste")
+						//.setDisabled(this.clipboardHasText())
+						.onClick(() => {
+							this.pasteWithoutNewlines(editor);
+						});
+				});
+			})
+		);
+
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	removeNewlines(text: string) {
+		if (this.settings.fixHyphenation) {
+			text = text.replace(/-\n/g, "");
+		}
+
+		text = text.replace(/\n/g, " ");
+
+		if (this.settings.fixWhitespace) {
+			text = text.replace(/\s{2,}/g, " ");
+		}
+
+		return text;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	removeNewlinesFromSelection(editor: Editor) {
+		let selection = editor.getSelection();
+
+		selection = this.removeNewlines(selection);
+
+		editor.replaceSelection(selection);
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	async pasteWithoutNewlines(editor: Editor) {
+		let selection = await navigator.clipboard.readText();
+
+		selection = this.removeNewlines(selection);
+
+		editor.replaceSelection(selection);
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: RemoveNewline;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: RemoveNewline) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		new Setting(containerEl).setName('Remove Newlines').setHeading();
+
+		//containerEl.createEl("h2", { text: "Remove Newlines From Selection" });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Fix whitespace")
+			.setDesc(
+				"Remove two or more whitespace characters in a row from the selection after removing the newlines. (Recommended)"
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.fixWhitespace)
+					.onChange(async (value) => {
+						this.plugin.settings.fixWhitespace = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Fix hyphenation")
+			.setDesc(
+				"If on, removes hyphens from the end of a line and also does not put a space where the newline was."
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.fixHyphenation)
+					.onChange(async (value) => {
+						this.plugin.settings.fixHyphenation = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
